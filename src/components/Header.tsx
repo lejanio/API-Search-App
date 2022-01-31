ac7a03eec3faef414d259a16815f20423f71222c
lejanio <janis.leonciks@gmail.com>
2022-01-31 12:11:27 +0200

    header type removed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,9 @@ import React, { FC } from 'react';
 import Logo from './Logo';
 import { StyledHeader } from './styles/Header.styles';
 
-type HeaderProps = {}
-
-const Header:FC<HeaderProps> = ({ children }) => (
+const Header = () => (
   <StyledHeader>
     <Logo />
-    {children}
   </StyledHeader>
 );
 
